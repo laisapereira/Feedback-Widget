234cb112c83c493135d518064c6620dace7fd9c7
Laisa Pereira <laisasilva.pereira@yahoo.com>
2022-05-17 21:42:20 -0300

    add css to botao feedback

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -4,7 +4,18 @@ import { ChatTeardropDots } from "phosphor-react"
 export function Widget() {
   return (
     
-      <h1 className="bg-red-500">Oioioi</h1>
+      <div className="absolute bottom-5 right-5 hover ">
+        <button className="bg-brand-500 rounded-full px-3 h-12 flex items-center group text-white">
+          <ChatTeardropDots className="w-6 h-6"/>
+
+          <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+            <span className="pl-2"></span>
+            Feedback
+          
+          
+          </span>
+        </button>
+      </div>
     
   )
 }
