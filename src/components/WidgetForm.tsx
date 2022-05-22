13a7822226638810f9d19811bb6d443794734d81
Laisa Pereira <laisasilva.pereira@yahoo.com>
2022-05-22 20:12:35 -0300

    adicionando botao de fechar o widget

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ClosedButton from './ClosedButton'
+
+export default function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className='text-xl leading-6'> Deixe seu feedback</span>
+        <ClosedButton />
+      </header>
+
+      <footer className='text-xs text-neutral-400'>
+        Feito com ♥ pela <a className="underline underline-offset-2"href="https://www.google.com/">BlaBlaBla</a>
+      </footer>
+    </div>
+  )
+}
